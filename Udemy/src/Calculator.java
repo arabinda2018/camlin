84f10a34a275c652b66a202f4bc8d234e22c47c9
Arabinda Mohanty <35189540+arabinda2018@users.noreply.github.com>
2020-01-05 14:40:05 -0800

    learn udemy

---
+++ Udemy/src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+public int getSum(int num1,int num2) {
+int result =num1+num2;
+	return result;
+}
+public int getSub(int num1,int num2) {
+	int result =num1=num2;
+	return result;
+}
+public int getMul(int num,int num2) {
+	int result =num*num2;
+	return  result;
+}
+}
---
+++ Udemy/src/calcproj/Calculator.java
@@ -0,0 +1,45 @@
+package calcproj;
+
+public class Calculator {
+	/*The dark night 
+	 * ctrl+shift+f
+	 * 
+	 */
+	public Calculator() {
+	System.out.println("calling constructor.");
+	
+	
+	}
+	public  void add() {
+		System.out.println("add ");
+	}
+	public void sub() {
+		System.out.println("sub");
+		
+	}
+	public void div() {
+		System.out.println("div");
+		
+	}
+	public void mul() {
+		System.out.println("mul");
+		
+	String value ="smith";
+	
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+	 new Calculator().add();
+	 new Calculator().sub();
+//		calc.add();
+//		calc.sub();
+//		calc.mul();
+//		calc.div();
+		
+	}
+
+}
