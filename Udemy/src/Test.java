af5703ab17352dda6f3a1f2c76375082c66c452f
Arabinda Mohanty <35189540+arabinda2018@users.noreply.github.com>
2020-01-05 14:44:02 -0800

    hi

---
+++ Udemy/src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+	// start of body
+	Test() {
+		System.out.println("hi");
+	}
+
+	Test(String a) {
+		System.out.println(" your name " + a);
+	}
+
+	Test(int a, int b) {
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+	public static void main(String[] args) {
+		Test t = new Test("arabinda");
+		Test t2 = new Test(10, 20); 
+	}
+
+}// end of body
