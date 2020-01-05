b7b1e7df2a024751799d81a1830fd714f599307e
Arabinda Mohanty <35189540+arabinda2018@users.noreply.github.com>
2020-01-05 14:45:48 -0800

    last commit

---
+++ Udemy/src/Loan.java
@@ -0,0 +1,16 @@
+
+public class Loan {
+	public static void main(String[] args) {
+
+	}
+
+	public void calculatePrincipalAmount() {
+		System.out.println("principal amount count");
+	}
+
+	public void loanAmount() {
+		
+		
+		System.out.println("loan amount ");
+	}
+}
